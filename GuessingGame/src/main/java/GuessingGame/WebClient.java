7b14ac9a56ae834f1394bb9a0d61da7b3d219a2e
SaraBertse <61227732+SaraBertse@users.noreply.github.com>
2021-11-23 10:21:59 +0100

    update Webclient

--- GuessingGame/src/main/java/GuessingGame/WebClient.java
+++ GuessingGame/src/main/java/GuessingGame/WebClient.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -22,9 +23,29 @@ public class WebClient {
             URL u;
             HttpURLConnection h;
             InputStream is;
-            u = new URL("http://127.0.0.1"); //https://www.kth.se
+            u = new URL("http://127.0.0.1:7070/index.html"); //https://www.kth.se
             h = (HttpURLConnection)u.openConnection();
+            h.setRequestProperty("Set-Cookie", "en-US");
             h.connect();
+           
+         
+            //Den tredje headern är userId=value cookien
+         /*   for(int i = 0; i<h.getHeaderFields().size(); i++){
+               cookie=(h.getHeaderField(i));
+            }*/
+         
+            String cookieStr=(h.getHeaderField(3));
+            StringTokenizer tokens = new StringTokenizer(cookieStr," =");
+            tokens.nextToken();
+            String cookie = tokens.nextToken();
+            System.out.println(cookie);
+            
+            
+            //Denna kan man använda för att skicka en POST
+            //h.setRequestMethod("POST");
+            
+            
+            
             //h.getHeaderField(name)
             is = h.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
